64d5f80d64b39715d640fb0f18988744de50666b
curious725 <curious725@gmail.com>
2017-07-16 15:03:28 +0300

    add results view and map it to url

--- polls/polls_app/urls.py
+++ polls/polls_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /polls/5
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 ]

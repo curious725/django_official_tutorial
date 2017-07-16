d1557279d0ca53e815fc8de4dc782660b0bb010e
curious725 <curious725@gmail.com>
2017-07-16 16:19:40 +0300

    update detail view to use detail.htlm template

--- polls/polls_app/views.py
+++ polls/polls_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 
 from .models import Question
 
@@ -15,8 +16,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(
-        "You're looking at question %s." % question_id
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(
+        request, 'polls_app/detail.html', {'question': question}
     )
 
 

b4c28a7d6b9dbb3927c1a1902cb38173e59c9379
curious725 <curious725@gmail.com>
2017-07-16 18:25:03 +0300

    rewrite vote() view

--- polls/polls_app/views.py
+++ polls/polls_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -28,6 +28,24 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(
-        "You're voting on question %s." % question_id
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(
+            pk=request.POST['choice']
+        )
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(
+            request, 'polls_app/detail.html',
+            {'question': question,
+             'error_message': "You didn't select a choice."}
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if
+    # a use hits a Back button.
+    return HttpResponseRedirect(
+        reverse('polls_app:results', args=(question.id,))
     )
